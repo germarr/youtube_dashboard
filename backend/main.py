e724ecf0836ec0393074407cb09f048b2ec4245a
Gerardo Martinez <gmarr@Gerardos-MacBook-Pro.local>
2021-04-11 20:10:28 -0500

    FastAPI file to manage the file APIS

--- backend/main.py
+++ backend/main.py
@@ -1,8 +1,27 @@
 from fastapi import FastAPI
 app = FastAPI()
+from data import main
+from comments import comments_analysis
+from test_script import testMain
 
 @app.get("/")
 def main_api():
     return {
         "message":"hello World"
+    }
+
+@app.get("/datos")
+def get_data(URL:str=None):
+    data = main(mainURL=URL)
+    
+    return{
+        "items":data
+    }
+
+@app.get("/datosV2")
+def test():
+    data = testMain()
+    
+    return{
+        "items":data
     }
